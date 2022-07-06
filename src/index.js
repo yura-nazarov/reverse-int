acd8351c6b573c228f6a31b602a5411990bf70d4
Yuri <yuriy.nazaro@gmail.com>
2022-07-06 23:16:18 +0500

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+
+    let newArr = [];
+
+    String(n).split('').map(elem => {
+        newArr.unshift(elem);
+    })
+
+    return newArr.join('');
 }
+
+// console.log(module.exports(-123));
